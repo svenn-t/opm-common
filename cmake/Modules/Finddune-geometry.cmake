2de2ceedb52d61eeb2834dcf6400262cd0cfa1ea
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-03-16 10:05:17 +0100

    Finddune-geometry: drop find_opm_package
    the HAVE_ALGLIB config variable is no longer used

--- cmake/Modules/Finddune-geometry.cmake
+++ cmake/Modules/Finddune-geometry.cmake
@@ -9,41 +9,18 @@
 
 # Copyright (C) 2013 Uni Research AS
 # This code is licensed under The GNU General Public License v3.0
+if(dune-geometry_FOUND)
+  return()
+endif()
 
-include (OpmPackage)
-find_opm_package (
-  # module name
-  "dune-geometry"
+if(dune-geometry_FIND_REQUIRED)
+  find_package(dune-geometry CONFIG REQUIRED)
+else()
+  find_package(dune-geometry CONFIG)
+endif()
 
-  # dependencies
-  # TODO: we should probe for all the HAVE_* values listed below;
-  # however, we don't actually use them in our implementation, so
-  # we just include them to forward here in case anyone else does
-  "dune-common REQUIRED
-  "
-  # header to search for
-  "dune/geometry/quadraturerules.hh"
-
-  # library to search for
-  "dunegeometry"
-
-  # defines to be added to compilations
-  ""
-
-  # test program
-"#include <dune/geometry/quadraturerules.hh>
-int main (void) {
-  Dune::GeometryType gt = Dune::GeometryTypes::quadrilateral;
-  Dune::QuadratureRules<double, 2>::rule(gt, 2).size();
-  return 0;
-}
-"
-  # config variables
-  "HAVE_ALGLIB
-  ")
-
-#debug_find_vars ("dune-geometry")
-
-# make version number available in config.h
-include (UseDuneVer)
-find_dune_version ("dune" "geometry")
+if(dune-geometry_FOUND)
+  # make version number available in config.h
+  include (UseDuneVer)
+  find_dune_version ("dune" "geometry")
+endif()
