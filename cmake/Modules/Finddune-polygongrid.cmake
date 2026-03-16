6e5bf244303e72aaa74972affb45f08b5e6e4114
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-03-16 10:05:17 +0100

    Finddune-polygongrid: drop find_opm_package

--- cmake/Modules/Finddune-polygongrid.cmake
+++ cmake/Modules/Finddune-polygongrid.cmake
@@ -1,4 +1,4 @@
-# - Find DUNE Fem library
+# - Find DUNE polygongrid library
 #
 # Defines the following variables:
 #   dune-polygongrid_INCLUDE_DIRS    Directory of header files
@@ -9,38 +9,18 @@
 
 # Copyright (C) 2015 IRIS AS
 # This code is licensed under The GNU General Public License v3.0
+if(dune-polygongrid_FOUND)
+  return()
+endif()
 
-include (OpmPackage)
-find_opm_package (
-  # module name
-  "dune-polygongrid"
+if(dune-polygongrid_FIND_REQUIRED)
+  find_package(dune-polygongrid CONFIG REQUIRED)
+else()
+  find_package(dune-polygongrid CONFIG)
+endif()
 
-  # dependencies
-  # TODO: we should probe for all the HAVE_* values listed below;
-  # however, we don't actually use them in our implementation, so
-  # we just include them to forward here in case anyone else does
-  "dune-common REQUIRED"
-  # header to search for
-  "dune/polygongrid/mesh.hh"
-
-  # library to search for
-  "dunepolygongrid"
-
-  # defines to be added to compilations
-  ""
-
-  # test program
-"#include <dune/polygongrid/mesh.hh>
-int main (void) {
-   return 0;
-}
-"
-# config variables
-  "HAVE_DUNE_POLYGONGRID
-  ")
-
-#debug_find_vars ("dune-polygongrid")
-
-# make version number available in config.h
-include (UseDuneVer)
-find_dune_version ("dune" "polygongrid")
+if(dune-polygongrid_FOUND)
+  # make version number available in config.h
+  include (UseDuneVer)
+  find_dune_version ("dune" "polygongrid")
+endif()
