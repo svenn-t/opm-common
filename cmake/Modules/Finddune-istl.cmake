ae43c8be63030b4e250c5d0c9f16d6afe54c8a88
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-03-16 10:05:17 +0100

    Finddune-istl: drop find_opm_package
    the still relevant config variables are handled by the associated targets

--- cmake/Modules/Finddune-istl.cmake
+++ cmake/Modules/Finddune-istl.cmake
@@ -9,59 +9,24 @@
 
 # Copyright (C) 2012 Uni Research AS
 # This code is licensed under The GNU General Public License v3.0
+if(dune-istl_FOUND)
+  return()
+endif()
 
-include (OpmPackage)
-find_opm_package (
-  # module name
-  "dune-istl"
+if(dune-istl_FIND_REQUIRED)
+  find_package(dune-istl CONFIG REQUIRED)
+else()
+  find_package(dune-istl CONFIG)
+endif()
 
-  # required dependencies
-  "dune-common REQUIRED;
-  ParMETIS;
-  SuperLU;
-  SuiteSparse COMPONENTS UMFPACK REQUIRED
-  "
-  # header to search for
-  "dune/istl/bcrsmatrix.hh"
+if(dune-istl_FOUND)
+  # make version number available in config.h
+  include (UseDuneVer)
+  find_dune_version ("dune" "istl")
 
-  # library to search for
-  ""
-
-  # defines to be added to compilations
-  ""
-
-  # test program
-"#include <dune/common/deprecated.hh>
-#include <dune/istl/bcrsmatrix.hh>
-#include <dune/common/fmatrix.hh>
-
-int main (void) {
-  typedef Dune::BCRSMatrix<Dune::FieldMatrix<double,1,1> > Matrix;
-  Matrix matrix( 3, 3, Matrix::random );
-  for (int i = 0; i < 3; ++i) matrix.setrowsize(i, 2);
-  matrix.endrowsizes();
-  return 0;
-}
-"
-  # config variables
-  "HAVE_BOOST_FUSION;
-  HAVE_MEM_USAGE_T_EXPANSIONS;
-  HAVE_PARDISO;
-  HAVE_BOOST;
-  HAVE_MPI;
-  HAVE_PARMETIS;
-  HAVE_SUPERLU;
-  HAVE_SUITESPARSE_UMFPACK;
-  SUPERLU_INT_TYPE;
-  ")
-#debug_find_vars ("dune-istl")
-
-# make version number available in config.h
-include (UseDuneVer)
-find_dune_version ("dune" "istl")
-
-if(dune-istl_FOUND AND NOT TARGET Dune::ISTL)
-  add_library(Dune::ISTL INTERFACE IMPORTED)
-  target_link_libraries(Dune::ISTL INTERFACE dunecommon)
-  target_include_directories(Dune::ISTL INTERFACE ${dune-istl_INCLUDE_DIRS})
+  if(NOT TARGET Dune::ISTL)
+    add_library(Dune::ISTL INTERFACE IMPORTED)
+    target_link_libraries(Dune::ISTL INTERFACE dunecommon)
+    target_include_directories(Dune::ISTL INTERFACE ${dune-istl_INCLUDE_DIRS})
+  endif()
 endif()
